d51e7025ad6d213b25ff85d5dd91d7aef8484109
Alenis Chavarria <112582758+alenischavarria@users.noreply.github.com>
2024-04-09 20:24:04 -0500

    added connections to landing page

---
+++ necessary.js
@@ -0,0 +1,11 @@
+function goToCustomerSignUp() {
+    window.location.href = 'client_signup.html';
+}
+
+function goToVendorSignUp() {
+    window.location.href = 'vendor_signup.html';
+}
+
+function goGetHelp() {
+    window.location.href = 'faq.html';
+}
