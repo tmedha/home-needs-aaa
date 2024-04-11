0fb6355a71360fccb2cd54043117cf4830e9262c
Alenis Chavarria <112582758+alenischavarria@users.noreply.github.com>
2024-04-10 20:31:06 -0500

    updating

---
+++ imgEXforServices/searchqueries.js
@@ -0,0 +1,4 @@
+function goToSearchResults() {
+    console.log("Function is called");
+    window.location.href = 'results.html';
+}
---
+++ review.js
@@ -0,0 +1,8 @@
+function goToWriteAReview() {
+    window.location.href = 'writing_reviews.html';
+}
+
+function submitReview(event){
+    event.preventDefault();
+    window.location.href = 'services.html';
+}
